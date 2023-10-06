78927d99b3b6af27e89bd68e2280b87a20223b0c
Andrey Marakulin <annndruha.github@gmail.com>
2023-10-06 23:20:26 +0500

    Add important note

--- hw1/protocol.py
+++ hw1/protocol.py
@@ -69,12 +69,15 @@ class DeBufferizer(BufferSettings):
                 self.total_parts = int(total_parts)
             else:
                 assert self.total_parts == int(total_parts)
+            print('TP', self.total_parts)
 
     def is_done(self) -> bool:
         return len(self.data_arr) == self.total_parts
 
-    def get_one_lost(self):
-        return set(range(1, self.total_parts+1)) - set(self.data_arr.keys())
+    def losts(self):
+        if self.total_parts is None:
+            return None
+        return list(set(range(1, self.total_parts+1)) - set(self.data_arr.keys()))
 
     def get_data(self):
         data = bytearray()
@@ -93,14 +96,44 @@ class MyTCPProtocol(UDPBasedProtocol):
         b = Bufferizer(data)
         for data_part in b:
             self.sendto(data_part)
+
+        # recv_done = False
+        # while not recv_done:
+        #     data = self.recvfrom(self.max_size)
+        #     if data.startswith(b'ASK'):
+        #         print('SENER NOTIFICATED')
+        #     elif data.startswith(b'DONE'):
+        #         recv_done = True
         return len(data)
 
     def recv(self, n: int):
         d = DeBufferizer()
-        while not d.is_done():
-            data_part = self.recvfrom(self.max_size)
+        data_part = self.recvfrom(self.max_size)
+        if not data_part.startswith(b'ASK') and not data_part.startswith(b'DONE'):
             d.add_part(data_part, self.seen_ids)
-        return d.get_data()
+            while len(d.losts()) > 0:
+                ask_part = bytearray()
+                ask_part.extend(b'ASK')
+                ask_part.extend(bytes(str(d.losts()[0]), encoding="UTF-8"))
+                self.sendto(ask_part)
+
+            return d.get_data()
+        elif data_part.startswith(b'ASK'):
+            return data_part
+        elif data_part.startswith(b'DONE'):
+            return data_part
+        
+        # while not d.is_done():
+        #     if len(d.get_one_lost()) > 0:
+        #         lost_part = d.get_one_lost()
+        #         ask_for_lost = bytearray()
+        #         ask_for_lost.extend(b'ASK')
+        #         ask_for_lost.extend(lost_part[0])
+        #         self.sendto(ask_for_lost)
+        #     else:
+        #         print('WTF???')
+        
+        
 
 
 if __name__ == "__main__":
@@ -112,8 +145,11 @@ if __name__ == "__main__":
     # setup_netem(packet_loss=0.00, duplicate=0.00, reorder=0.00)
     # run_echo_test(iterations=2, msg_size=16)
 
-    setup_netem(packet_loss=0.0, duplicate=0.02, reorder=0.0)
-    run_echo_test(iterations=5000, msg_size=14)
+    # setup_netem(packet_loss=0.0, duplicate=0.02, reorder=0.0)
+    # run_echo_test(iterations=5000, msg_size=14)
+
+    setup_netem(packet_loss=0.7, duplicate=0.02, reorder=0.01)
+    run_echo_test(iterations=2, msg_size=10)
 
     # seen_ids = []
     # msg = b'\xdc\xf5\x06P\xce\x9eZ\xd9\xcf\x10\xa5\xa4\r'
@@ -125,3 +161,7 @@ if __name__ == "__main__":
     #     d.add_part(part, seen_ids)
     # assert msg == d.get_data()
     # print(d.is_done())
+
+
+# Сброс кривых параметров (из-за которых в том числе может отваливаться VSCode remote)
+# tc qdisc replace dev lo root netem loss 0% duplicate 0% reorder 0% delay 0ms
