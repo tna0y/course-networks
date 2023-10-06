52bb95eab74cde7763bc35d5a5d8936172871f49
Andrey Marakulin <annndruha.github@gmail.com>
2023-10-06 19:35:12 +0500

    Norm

--- hw1/protocol.py
+++ hw1/protocol.py
@@ -27,7 +27,6 @@ class Bufferizer:
         self.sn_sep = bytes('/', encoding="UTF-8")
         self.data = data
         self.data_len = len(data)
-        self.data_id = os.urandom(48)
         if self.data_len % self.window_size != 0:
             self.total_parts = self.data_len // self.window_size + 1
         else:
@@ -39,7 +38,7 @@ class Bufferizer:
         part.extend(self.sn_sep)
         part.extend(bytes(str(self.total_parts), encoding="UTF-8"))
         part.extend(self.magic_sep)
-        part.extend(self.data_id)
+        part.extend(os.urandom(48))
         part.extend(self.magic_sep)
         part.extend(self.data[i*self.window_size:(i+1)*self.window_size])
         return part
@@ -57,24 +56,26 @@ class DeBufferizer:
         self.sn_sep = bytes('/', encoding="UTF-8")
         self.data_arr = {}
         self.total_parts = None
-        self.data_id = None
 
     def add_part(self, unknown_part):
         meta, _, part_data = unknown_part.partition(self.magic_seq)
-
         sequence_number, _, total_parts = meta.partition(self.sn_sep)
+        data_id, _, part_data = part_data.partition(self.magic_seq)
+
+        print(f"REC PART ===", data_id.hex(), int(sequence_number),"/", int(total_parts), part_data.hex())
+
+        self.data_arr[int(sequence_number)] = part_data
+
         if self.total_parts is None:
             self.total_parts = int(total_parts)
         else:
             assert self.total_parts == int(total_parts)
-
+    
+    def get_id(self, unknown_part):
+        meta, _, part_data = unknown_part.partition(self.magic_seq)
+        sequence_number, _, total_parts = meta.partition(self.sn_sep)
         data_id, _, part_data = part_data.partition(self.magic_seq)
-
-        if self.data_id is None:
-            self.data_id = data_id
-        else:
-            if self.data_id == data_id:
-                self.data_arr[int(sequence_number)] = part_data
+        return data_id.hex()
 
     def is_done(self) -> bool:
         return len(self.data_arr) == self.total_parts
@@ -82,11 +83,13 @@ class DeBufferizer:
     def get_losts(self):
         return set(range(1, self.total_parts+1)) - set(self.data_arr.keys())
 
-
     def get_data(self):
         data = bytearray()
         for i in range(1, self.total_parts+1):
             data.extend(self.data_arr[i])
+        self.data_arr = {}
+        self.total_parts = None
+        # self.seen_ids = []
         return bytes(data)
 
 
@@ -94,12 +97,14 @@ class MyTCPProtocol(UDPBasedProtocol):
     def __init__(self, *args, **kwargs):
         self.max_size = 2 ** 32
         super().__init__(*args, **kwargs)
+        self.seen_ids = []
 
     def send(self, data: bytes):
         b = Bufferizer(data)
         for data_part in b:
+            print(f"NEW PART ===", data.hex())
             self.sendto(data_part)
-            print("========SEND:", bytes(data_part))
+            self.sendto(data_part)
         # self.sendto(data)
         # print("========SEND============", bytes(data))
         return len(data)
@@ -108,8 +113,10 @@ class MyTCPProtocol(UDPBasedProtocol):
         d = DeBufferizer()
         while not d.is_done():
             data_part = self.recvfrom(self.max_size)
-            print("========RECV:", bytes(data_part))
-            d.add_part(data_part)
+            part_id = d.get_id(data_part)
+            if part_id not in self.seen_ids:
+                d.add_part(data_part)
+                self.seen_ids.append(part_id)
         return d.get_data()
         # data = self.recvfrom(self.max_size)
         # print("========RECV============", bytes(data))
@@ -123,7 +130,7 @@ if __name__ == "__main__":
     # run_echo_test(iterations=2, msg_size=msg_size)
 
 
-    setup_netem(packet_loss=0.0, duplicate=0.1, reorder=0.0)
+    setup_netem(packet_loss=0.0, duplicate=0.9, reorder=0.0)
     run_echo_test(iterations=10000, msg_size=14)
 
 
