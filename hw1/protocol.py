89e1afd99721cc13db8a9fe4261f01d2aa9312be
Andrey Marakulin <annndruha.github@gmail.com>
2023-10-09 12:04:37 +0500

    Final cool soultion with logs

--- hw1/protocol.py
+++ hw1/protocol.py
@@ -7,7 +7,9 @@ import functools
 import logging
 
 logging.basicConfig(format='%(message)s',
-                    level=logging.INFO)
+                    level=logging.INFO,
+                    handlers=[logging.FileHandler("main_double.logs"),
+                              logging.StreamHandler()])
 
 
 class UDPBasedProtocol:
@@ -102,19 +104,27 @@ class MyTCPProtocol(UDPBasedProtocol):
         self.recv_buffer = []
 
     def sendto(self, send_data: bytes, who_am_i: str = 'unknown'):
-        logging.info(who_am_i + str(bytes(send_data)))
+        logging.info(who_am_i + ' SEND ' + str(bytes(send_data)))
         super().sendto(send_data)
 
     def recvfrom(self, send_data: bytes, who_am_i: str = 'unknown'):
         data = super().recvfrom(send_data)
-        logging.info(who_am_i + str(bytes(data)))
+        logging.info(who_am_i + ' GETS ' + str(bytes(data)))
         return data
 
     def send(self, send_data: bytes, who_am_i: str = 'unknown'):
+        who_am_i += 'send '
         b = Bufferizer(send_data)
         self.send_buffer[b.id] = b
         for part in b:
             self.sendto(part, who_am_i)
+        #     self.sendto(part, who_am_i)
+
+        # data = self.recvfrom(self.max_size, who_am_i)
+        # if data.startswith(b'OK'):
+        #     okid = data.removeprefix(b'OK')
+        #     if okid in self.send_buffer.keys():
+        #         del_item = self.send_buffer.pop(okid)
 
         while len(self.send_buffer):
             try:
@@ -128,7 +138,6 @@ class MyTCPProtocol(UDPBasedProtocol):
                     else:
                         logging.info(who_am_i + 'Duplicate ok')
                 elif data.startswith(b'GET'):
-                    logging.info(who_am_i + 'get')
                     try:
                         _, id, lost_part = data.split(SEP)
                         if id == b'NEW':
@@ -139,12 +148,10 @@ class MyTCPProtocol(UDPBasedProtocol):
                             self.sendto(self.send_buffer[id][int(lost_part)], who_am_i)
                     except KeyError:
                         logging.info(who_am_i + 'Key')
-                        pass
                 elif data.startswith(b'APPROVE'):
                     id = data.removeprefix(b'APPROVE')
                     if id in self.recv_buffer:
                         self.sendto(b'OK' + id, who_am_i)
-                        logging.info(who_am_i + 'break loop')
                     else:
                         logging.info(who_am_i + 'not in buffer' + str(id))
                 elif data.startswith(b'DATA'):
@@ -152,15 +159,16 @@ class MyTCPProtocol(UDPBasedProtocol):
                 else:
                     logging.info(who_am_i + 'WTF2' + str(data))
             except TimeoutError:
+                logging.info(who_am_i + 'ToE')
                 for part in b:
                     self.sendto(part, who_am_i)
                 self.sendto(b'APPROVE' + b.id, who_am_i)
-                logging.info(who_am_i + 'ToE')
 
         logging.info(who_am_i + 'Closed')
         return len(send_data)
 
     def recv(self, n: int, who_am_i: str = 'unknown'):
+        who_am_i += 'recv '
         d = DeBufferizer()
         while not d.is_done():
             try:
@@ -174,7 +182,7 @@ class MyTCPProtocol(UDPBasedProtocol):
                     else:
                         self.sendto(b'GET' + SEP + b'NEW' + SEP + b'_', who_am_i)
                 elif data.startswith(b'GET'):
-                    logging.info(who_am_i + 'here')
+                    logging.info(who_am_i + 'pass recv get')
                 elif data.startswith(b'OK'):
                     okid = data.removeprefix(b'OK')
                     self.recv_buffer.append(okid)
@@ -200,10 +208,11 @@ if __name__ == "__main__":
     from protocol_test import *
 
     t = time.time()
-    for iterations in [10, 100, 1000]:
+    for iterations in [1000]:
         setup_netem(packet_loss=0.1, duplicate=0.0, reorder=0.0)
         run_echo_test(iterations=iterations, msg_size=17)
-    print(time.time() - t)
+    logging.info('=============')
+    logging.info(time.time() - t)
 
 # Сброс кривых параметров (из-за которых в том числе может отваливаться VSCode remote)
 # tc qdisc replace dev lo root netem loss 0% duplicate 0% reorder 0% delay 0ms
